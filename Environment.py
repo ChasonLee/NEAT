af16ee2abf388a03328da89e8bd4bf90d9a8b90a
Chason <329546652@qq.com>
2017-02-17 00:01:32 +0800

    Added genome mating function

--- Environment.py
+++ Environment.py
@@ -2,6 +2,7 @@
 __author__ = 'Chason'
 
 from NEAT import NEAT
+import random
 
 class Environment(object):
     """This is an ecological environment that can control the propagation of evolutionary neural networks.
@@ -20,8 +21,6 @@ class Environment(object):
         self.genomes = [NEAT(i, input_size, output_size) for i in range(population_size)]
         self.outcomes = []
         self.generation_iter = 0
-    def mating_genomes(self):
-        pass
 
     def add_outcome(self, genome):
         self.outcomes.append(genome)
@@ -30,23 +29,52 @@ class Environment(object):
                                                                                        len(genome.hidden_nodes),
                                                                                        genome.connection_count())
 
+    def mating_pair(self, pair):
+        p1 = pair[0].connections.sort(key=lambda Connection:Connection.innovation)
+        p2 = pair[1].connections.sort(key=lambda Connection:Connection.innovation)
+        p1_len = len(p1)
+        p2_len = len(p2)
+        max_len = max(p1_len, p2_len)
+        # to be continue
+
+    def mating_genomes(self):
+        mating_pool = []
+        for k, gen in enumerate(self.genomes):
+            if NEAT.probability(0.01 * gen.fitness):
+                mating_pool.append(gen)
+
+        while len(mating_pool) > 0:
+            pair = random.sample(mating_pool, 2)
+            self.mating_pair(pair)
+            for p in pair:
+                mating_pool.remove(p)
+
+    def mutation(self, task):
+        for k, gen in enumerate(self.genomes):
+            gen.mutation()
+            task.xor_fitness(gen)
+            # collecting outcomes
+            if gen.fitness == task.best_fitness:
+                self.add_outcome(gen)
+                self.genomes[k] = NEAT(gen.id, self.input_size, self.output_size)
+
+    def kill_bad_genomes(self):
+        for k, gen in enumerate(self.genomes):
+            if gen.fitness <= 1 and len(gen.hidden_nodes) > 1:
+                self.genomes[k] = NEAT(gen.id, self.input_size, self.output_size)
+
     def run(self, task, showResult=False):
         """Run the environment."""
         print "Running Environment...(population size = %d, max generation = %d)"%(self.population_size, self.max_generation)
-        completed_genomes = []
+        # generational change
         for self.generation_iter in range(self.max_generation):
+
             # mutation
-            for k,gen in enumerate(self.genomes):
-                gen.mutation()
-                task.xor_fitness(gen)
-                # collecting outcomes
-                if  gen.fitness == task.best_fitness:
-                    self.add_outcome(gen)
-                    self.genomes[k] = NEAT(gen.id, self.input_size, self.output_size)
+            self.mutation(task)
+
             # killing bad genomes
-            for k, gen in enumerate(self.genomes):
-                if gen.fitness <= 1 and len(gen.hidden_nodes) > 1:
-                    self.genomes[k] = NEAT(gen.id, self.input_size, self.output_size)
+            self.kill_bad_genomes()
+
         if showResult:
             print "Completed Genomes:"
             self.outcomes.sort(key=lambda NEAT:NEAT.hidden_nodes)
